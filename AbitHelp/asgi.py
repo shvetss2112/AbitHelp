3226440c2c54fa28d656c29f82d9d2cc2d2a59bf
Macht <nehreckystepan54@gmail.com>
2024-11-05 08:05:32 +0200

    New name AbitHelp

--- AbitHelp/asgi.py
+++ AbitHelp/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for abihelp project.
+ASGI config for AbitHelp project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'abihelp.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'AbitHelp.settings')
 
 application = get_asgi_application()
--- AbitHelp/wsgi.py
+++ AbitHelp/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for abihelp project.
+WSGI config for AbitHelp project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'abihelp.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'AbitHelp.settings')
 
 application = get_wsgi_application()
