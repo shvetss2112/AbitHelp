d8a41b3da68807727f8874c9a040cee7223ffb91
orest-sh <orest.sidliar@lnu.edu.ua>
2025-02-21 15:36:07 +0200

    add Event and Resource models to events app

--- events/models.py
+++ events/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Event(models.Model):
+    content = models.TextField(max_length=1500)
+    post_link = models.URLField()
+    date = models.DateTimeField(null=True, blank=True)
+    source = models.ForeignKey("Resource", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.content if len(self.content) <= 10 else f"{self.content[:10]}..."
+
+
+class Resource(models.Model):
+    name = models.CharField(max_length=64, null=False)
+    link = models.URLField()
+
+    def __str__(self):
+        return self.name
