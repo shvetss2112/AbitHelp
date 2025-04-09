6e32a60cc4b37ffee51c787e2521df1b20a570a4
orest-sh <orest.sidliar@lnu.edu.ua>
2025-04-09 17:14:37 +0300

    feat: add Django filter backend to EventViewSet and update settings

--- events/views.py
+++ events/views.py
@@ -2,9 +2,12 @@ from rest_framework import viewsets
 from .models import Event
 from .serializers import EventSerializer
 from .permissions import IsAdminOrReadOnly
+from rest_framework import filters
 
 
 class EventViewSet(viewsets.ModelViewSet):
     queryset = Event.objects.all()
     serializer_class = EventSerializer
     permission_classes = [IsAdminOrReadOnly]
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['content']
