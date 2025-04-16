67e955279cefeaeb09d460ec7f7f88da1fa427e2
shvetss2112 <shvetss2112@gmail.com>
2025-04-16 23:55:29 +0300

    :poop: add event list plug

--- event_calendar/static/main.js
+++ event_calendar/static/main.js
@@ -1,8 +1,9 @@
 'use strict';
-import { EventCalendar} from "./calendar.js";
-import {EventList} from "./event_list.js";
+import { EventCalendar, EventList } from "./calendar.js";
 
 let calendar_container = document.getElementById("cal-container");
-let calendar = new EventCalendar(calendar_container);
+let event_list_container = document.getElementById("cal-events-container");
 
-let event_list_container;
+let calendar = new EventCalendar(calendar_container);
+let event_list = new EventList(event_list_container);
+calendar.addEventList(event_list);
