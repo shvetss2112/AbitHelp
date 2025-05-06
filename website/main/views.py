e402a038123ec2ea3b19822d57f4d593c41e854f
orest-sh <orest.sidliar@lnu.edu.ua>
2025-05-06 14:11:22 +0300

    feat: implement search functionality in my_news view

--- website/main/views.py
+++ website/main/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from events.models import Event
 from django.core.paginator import Paginator
+from django.db.models import Q
 
 
 def index(request):
@@ -12,10 +13,18 @@ def about_us(request):
 
 
 def my_news(request):
-    news = Event.objects.all().order_by('-created_at')
-    paginator = Paginator(news, 10)
+    search_term = request.GET.get("search")
+    queryset = Event.objects
 
+    if search_term:
+        queryset = queryset.filter(
+            Q(content__icontains=search_term) | Q(title__icontains=search_term)
+        )
+
+    queryset = queryset.order_by('-created_at')
+
+    paginator = Paginator(queryset, 10)
     page_number = request.GET.get("page")
     page_obj = paginator.get_page(page_number)
 
-    return render(request, 'my_news.html', {"page_obj": page_obj})
+    return render(request, 'my_news.html', {"page_obj": page_obj, "search_term": search_term})
