e0e35c73ad4ad192917d4e402ab8ffce1190308f
Nick <Mykola.Biloshapka@lnu.edu.ua>
2025-07-06 21:26:33 +0300

    fix: fix filters file format

--- website/events/filters.py
+++ website/events/filters.py
@@ -4,29 +4,29 @@ from .models import Event
 
 
 class EventAPIFilter(django_filters.FilterSet):
-    search = django_filters.CharFilter(method='filter_search')
-    resource = django_filters.CharFilter(field_name='source__name', lookup_expr='iexact')
-    date__gte = django_filters.DateFilter(field_name='date', lookup_expr='gte')
-    date__lte = django_filters.DateFilter(field_name='date', lookup_expr='lte')
-    exclude = django_filters.CharFilter(method='filter_exclude')
-    include_any = django_filters.CharFilter(method='filter_include_any')
+	search = django_filters.CharFilter(method='filter_search')
+	resource = django_filters.CharFilter(field_name='source__name', lookup_expr='iexact')
+	date__gte = django_filters.DateFilter(field_name='date', lookup_expr='gte')
+	date__lte = django_filters.DateFilter(field_name='date', lookup_expr='lte')
+	exclude = django_filters.CharFilter(method='filter_exclude')
+	include_any = django_filters.CharFilter(method='filter_include_any')
 
-    class Meta:
-        model = Event
-        fields = ['resource', 'date__gte', 'date__lte', 'exclude', 'include_any']
+	class Meta:
+		model = Event
+		fields = ['resource', 'date__gte', 'date__lte', 'exclude', 'include_any']
 
-    def filter_search(self, queryset, name, value):
-        return queryset.filter(Q(title__icontains=value) | Q(content__icontains=value))
+	def filter_search(self, queryset, name, value):
+		return queryset.filter(Q(title__icontains=value) | Q(content__icontains=value))
 
-    def filter_exclude(self, queryset, name, value):
-	words = [word for word in value.split() if word]
-	for word in words:
-            queryset = queryset.exclude(Q(title__icontains=word) | Q(content__icontains=word))
-        return queryset
+	def filter_exclude(self, queryset, name, value):
+		words = [word for word in value.split() if word]
+		for word in words:
+        		queryset = queryset.exclude(Q(title__icontains=word) | Q(content__icontains=word))
+		return queryset
 
-    def filter_include_any(self, queryset, name, value):
-        q = Q()
-	words = [word for word in value.split() if word]
-	for word in words:
-		q |= Q(title__icontains=word) | Q(content__icontains=word)
-        return queryset.filter(q)
+	def filter_include_any(self, queryset, name, value):
+		q = Q()
+		words = [word for word in value.split() if word]
+		for word in words:
+			q |= Q(title__icontains=word) | Q(content__icontains=word)
+		return queryset.filter(q)
